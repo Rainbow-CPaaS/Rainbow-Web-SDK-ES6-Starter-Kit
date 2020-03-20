6aaf47634194512176c89e53fca872a0e33d982c
Konrad Hyzy <konrad.hyzy@al-enterprise.com>
2020-03-20 15:28:35 +0100

    Add server to test the application

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 import rainbowSDK from "./rainbow-sdk.min.js";
+import { AuthKeysNet, AuthKeysProd, AuthKeysSandbox } from "./AuthKeys.js";
 
 window.addEventListener("DOMContentLoaded", event => {
   console.log("*** HELLO, WORLD! - Rainbow Web SDK ***");
 
-  console.log(rainbowSDK);
   let onReady = function onReady() {
     console.log("[Hello World] :: On SDK Ready !");
   };
@@ -12,7 +12,7 @@ window.addEventListener("DOMContentLoaded", event => {
     console.log("[Hello World] :: On SDK Loaded !");
 
     rainbowSDK
-      .initialize("AppID", "AppSecret")
+      .initialize(AuthKeysProd.applicationID, AuthKeysProd.applicationSecret)
       .then(() => {
         console.log("Rainbow SDK is initialized!");
       })
